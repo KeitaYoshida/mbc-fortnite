a4d66a7c2b9206713aff286b83af6b48b82bc57b
KeitaYoshida <ba0503naky@gmail.com>
2021-02-11 14:46:50 +0900

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+  created: async () => {
+    // const url = "http://localhost:8000/items/itemlist";
+    // const res = await axios.get(url);
+    // console.log(res.data);
+    const apiUrl =
+      "https://public-api.tracker.gg/v2/fortnite/standard/profile/pc/MBC_KEITA";
+    const apiKey = "3912364f-1c9a-48d2-b117-1ddcd4ebd075";
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        "TRN-Api-Key": apiKey,
+      },
+    };
+    const response = await axios.post(apiUrl, config);
+    console.log(response.data);
+  },
+};
+</script>
